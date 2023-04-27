a8ea1fe1e801b6cd62e611ec75a3866dc76a32ac
Markiro <65sumo@gmail.com>
2023-04-27 12:36:36 +0300

    - добавлено новое исключение для вакансий

---
+++ src/main/java/com/example/helpMAMOCHKA/exceptions/VacationNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.helpMAMOCHKA.exceptions;
+
+public class VacationNotFoundException extends RuntimeException {
+    public VacationNotFoundException(String message) {
+        super(message);
+    };
+}
